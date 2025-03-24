2b4eb1e28084fd33a87cd877951b66b54a91d10a
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-24 14:15:18 +0100

    basic io

--- src/main.zig
+++ src/main.zig
@@ -12,4 +12,35 @@ pub fn main() !void {
     try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
     try posix.bind(listener, &address.any, address.getOsSockLen());
     try posix.listen(listener, 128);
+
+    const isRunning: bool = true;
+    while (isRunning) {
+        var clientAddress: net.Address = undefined;
+        var addressLen: posix.socklen_t = @sizeOf(net.Address);
+
+        const socket = posix.accept(listener, &clientAddress.any, &addressLen, 0) catch |err| {
+            std.debug.print("Connection acception error\n{}\n", .{err});
+            continue;
+        };
+        defer posix.close(socket);
+
+        std.debug.print("{} connected\n", .{clientAddress});
+
+        write(socket, "Hello!\n") catch |err| {
+            std.debug.print("Error writing\n{}\n", .{err});
+        };
+    }
+}
+
+fn write(socket: posix.socket_t, msg: []const u8) !void {
+    var pos: usize = 0;
+
+    while (pos < msg.len) {
+        const written = try posix.write(socket, msg[pos..]);
+        if (written == 0) {
+            return error.Closed;
+        }
+
+        pos += written;
+    }
 }
