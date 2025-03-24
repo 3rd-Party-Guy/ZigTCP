b5207fd0762f94534069461e7435a70548c2bd7e
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-24 13:59:39 +0100

    make socket listener

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,10 @@ pub fn main() !void {
     const address: net.Address = try std.net.Address.resolveIp("127.0.0.1", 6969);
     const tpe: u32 = posix.SOCK.STREAM;
     const protocol = posix.IPPROTO.TCP;
-
     const listener = try posix.socket(address.any.family, tpe, protocol);
-
     defer posix.close(listener);
+
+    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+    try posix.bind(listener, &address.any, address.getOsSockLen());
+    try posix.listen(listener, 128);
 }
