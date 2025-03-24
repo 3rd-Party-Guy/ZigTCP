68988f4ba203e30a5fa4762a3c1d9313f2b237df
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-24 13:52:42 +0100

    create basic socket

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
+const net = std.net;
+const posix = std.posix;
 
-pub fn main() void {
-    std.debug.print("Hello, {s}{s}\n", .{ "World", "!" });
+pub fn main() !void {
+    const address: net.Address = try std.net.Address.resolveIp("127.0.0.1", 6969);
+    const tpe: u32 = posix.SOCK.STREAM;
+    const protocol = posix.IPPROTO.TCP;
+
+    const listener = try posix.socket(address.any.family, tpe, protocol);
+
+    defer posix.close(listener);
 }
